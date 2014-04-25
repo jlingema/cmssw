bcce93759dab589bf477d1307339f144f6dca0de
aelwood <adam.elwood09@imperial.ac.uk>
2014-04-25 15:06:28 +0200

    Undid the pre6 bollocks, nice one Matt

--- L1Trigger/L1TCalorimeter/interface/CaloStage2JetAlgorithmFirmware.h
+++ L1Trigger/L1TCalorimeter/interface/CaloStage2JetAlgorithmFirmware.h
@@ -5,7 +5,7 @@
 ///    Concrete firmware implementations
 ///
 /// \author: Jim Brooke - University of Bristol
-///
+/// Modified: Adam Elwood - ICL
 
 //
 //
@@ -40,7 +40,37 @@ namespace l1t {
     CaloParams* const params_;
     
   };
-  
+
+  // Imp2 is Jad's algorithm with possible donut subtraction
+  class CaloStage2JetAlgorithmFirmwareImp2 : public CaloStage2JetAlgorithm {
+  public:
+    CaloStage2JetAlgorithmFirmwareImp2(CaloParams* params);
+    virtual ~CaloStage2JetAlgorithmFirmwareImp2();
+    virtual void processEvent(const std::vector<CaloTower> & towers,
+			      std::vector<Jet> & jets);
+
+    void create(const std::vector<CaloTower> & towers,
+		std::vector<Jet> & jets);
+
+    void getRing(int32_t shift, std::vector<std::vector<int> > & ring, 
+        const std::vector<CaloTower> & towers, std::vector<Jet> & jets);
+    
+    void filter(std::vector<Jet> & jets);
+    
+    void sort(std::vector<Jet> & jets);
+
+    bool jetIsZero(l1t::Jet jet);
+
+  private:
+
+    CaloParams* const params_;
+    int etaMin_ = -27;
+    int etaMax_ = 27;
+    int phiMin_ = 1;
+    int phiMax_ = 72;
+    
+  };
+
 }
 
 #endif
