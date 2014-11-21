13a867e63a5f36f0cf6af1e329f503f685b39900
Joschka Lingemann <joschka.lingemann@cern.ch>
2014-11-21 10:14:15 +0100

    Changed paths to lut configs to global paths to make it run from any directory.

--- L1Trigger/L1TGlobalMuon/python/microgmtemulator_cfi.py
+++ L1Trigger/L1TGlobalMuon/python/microgmtemulator_cfi.py
@@ -1,152 +1,157 @@
 import FWCore.ParameterSet.Config as cms
 
-microGMTEmulator = cms.EDProducer('MicroGMTEmulator',
-    barrelTFInput = cms.InputTag("microGMTInputProducer", "BarrelTFMuons"),
-    overlapTFInput = cms.InputTag("microGMTInputProducer", "OverlapTFMuons"),
-    forwardTFInput = cms.InputTag("microGMTInputProducer", "ForwardTFMuons"),
-    triggerTowerInput = cms.InputTag("microGMTInputProducer", "TriggerTowerSums"),
+import os
+
+l1tgmt_basedir = os.path.join(os.environ['CMSSW_BASE'], "src/L1Trigger/L1TGlobalMuon/")
+lut_dir = os.path.join(l1tgmt_basedir, "test/lut_configs/")
+
+microGMTEmulator = cms.EDProducer('l1t::MicroGMTEmulator',
+    barrelTFInput = cms.InputTag("uGMTInputProducer", "BarrelTFMuons"),
+    overlapTFInput = cms.InputTag("uGMTInputProducer", "OverlapTFMuons"),
+    forwardTFInput = cms.InputTag("uGMTInputProducer", "ForwardTFMuons"),
+    triggerTowerInput = cms.InputTag("uGMTInputProducer", "TriggerTowerSums"),
  
     AbsIsoCheckMemLUTSettings = cms.PSet (
-     	areaSum_in_width = cms.int32(5), 
-     	out_width = cms.int32(1),
-     	filename = cms.string('data/AbsIsoCheckMem.lut'),
+        areaSum_in_width = cms.int32(5), 
+        out_width = cms.int32(1),
+        filename = cms.string(os.path.join(lut_dir, 'AbsIsoCheckMem.lut')),
      ) ,
       
     IdxSelMemPhiLUTSettings = cms.PSet (
-     	phi_in_width = cms.int32(10), 
-     	out_width = cms.int32(6),
-     	filename = cms.string('data/IdxSelMemPhi.lut'),
+        phi_in_width = cms.int32(10), 
+        out_width = cms.int32(6),
+        filename = cms.string(os.path.join(lut_dir, 'IdxSelMemPhi.lut')),
      ) ,
       
     FwdPosSingleMatchQualLUTSettings = cms.PSet (
-     	deltaEtaRed_in_width = cms.int32(4), 
-     	deltaPhiRed_in_width = cms.int32(3), 
-     	out_width = cms.int32(1),
-     	filename = cms.string('data/FwdPosSingleMatchQual.lut'),
+        deltaEtaRed_in_width = cms.int32(4), 
+        deltaPhiRed_in_width = cms.int32(3), 
+        out_width = cms.int32(1),
+        filename = cms.string(os.path.join(lut_dir, 'FwdPosSingleMatchQual.lut')),
      ) ,
       
     BONegMatchQualLUTSettings = cms.PSet (
-     	deltaEtaRed_in_width = cms.int32(4), 
-     	deltaPhiRed_in_width = cms.int32(3), 
-     	out_width = cms.int32(1),
-     	filename = cms.string('data/BONegMatchQual.lut'),
+        deltaEtaRed_in_width = cms.int32(4), 
+        deltaPhiRed_in_width = cms.int32(3), 
+        out_width = cms.int32(1),
+        filename = cms.string(os.path.join(lut_dir, 'BONegMatchQual.lut')),
      ) ,
       
     OvlNegSingleMatchQualLUTSettings = cms.PSet (
-     	deltaEtaRed_in_width = cms.int32(4), 
-     	deltaPhiRed_in_width = cms.int32(3), 
-     	out_width = cms.int32(1),
-     	filename = cms.string('data/OvlNegSingleMatchQual.lut'),
+        deltaEtaRed_in_width = cms.int32(4), 
+        deltaPhiRed_in_width = cms.int32(3), 
+        out_width = cms.int32(1),
+        filename = cms.string(os.path.join(lut_dir, 'OvlNegSingleMatchQual.lut')),
      ) ,
       
     IdxSelMemEtaLUTSettings = cms.PSet (
-     	eta_in_width = cms.int32(9), 
-     	out_width = cms.int32(5),
-     	filename = cms.string('data/IdxSelMemEta.lut'),
+        eta_in_width = cms.int32(9), 
+        out_width = cms.int32(5),
+        filename = cms.string(os.path.join(lut_dir, 'IdxSelMemEta.lut')),
      ) ,
       
     FOPosMatchQualLUTSettings = cms.PSet (
-     	deltaEtaRed_in_width = cms.int32(4), 
-     	deltaPhiRed_in_width = cms.int32(3), 
-     	out_width = cms.int32(1),
-     	filename = cms.string('data/FOPosMatchQual.lut'),
+        deltaEtaRed_in_width = cms.int32(4), 
+        deltaPhiRed_in_width = cms.int32(3), 
+        out_width = cms.int32(1),
+        filename = cms.string(os.path.join(lut_dir, 'FOPosMatchQual.lut')),
      ) ,
       
     FwdNegSingleMatchQualLUTSettings = cms.PSet (
-     	deltaEtaRed_in_width = cms.int32(4), 
-     	deltaPhiRed_in_width = cms.int32(3), 
-     	out_width = cms.int32(1),
-     	filename = cms.string('data/FwdNegSingleMatchQual.lut'),
+        deltaEtaRed_in_width = cms.int32(4), 
+        deltaPhiRed_in_width = cms.int32(3), 
+        out_width = cms.int32(1),
+        filename = cms.string(os.path.join(lut_dir, 'FwdNegSingleMatchQual.lut')),
      ) ,
       
     BPhiExtrapolationLUTSettings = cms.PSet (
-     	charge_in_width = cms.int32(1), 
-     	etaAbs_in_width = cms.int32(8), 
-     	pTred_in_width = cms.int32(5), 
-     	out_width = cms.int32(4),
-     	filename = cms.string('data/BPhiExtrapolation.lut'),
+        charge_in_width = cms.int32(1), 
+        etaAbs_in_width = cms.int32(8), 
+        pTred_in_width = cms.int32(5), 
+        out_width = cms.int32(4),
+        filename = cms.string(os.path.join(lut_dir, 'BPhiExtrapolation.lut')),
      ) ,
       
     BrlSingleMatchQualLUTSettings = cms.PSet (
-     	deltaEtaRed_in_width = cms.int32(4), 
-     	deltaPhiRed_in_width = cms.int32(3), 
-     	out_width = cms.int32(1),
-     	filename = cms.string('data/BrlSingleMatchQual.lut'),
+        deltaEtaRed_in_width = cms.int32(4), 
+        deltaPhiRed_in_width = cms.int32(3), 
+        out_width = cms.int32(1),
+        filename = cms.string(os.path.join(lut_dir, 'BrlSingleMatchQual.lut')),
      ) ,
       
     RelIsoCheckMemLUTSettings = cms.PSet (
-     	areaSum_in_width = cms.int32(5), 
-     	pT_in_width = cms.int32(9), 
-     	out_width = cms.int32(1),
-     	filename = cms.string('data/RelIsoCheckMem.lut'),
+        areaSum_in_width = cms.int32(5), 
+        pT_in_width = cms.int32(9), 
+        out_width = cms.int32(1),
+        filename = cms.string(os.path.join(lut_dir, 'RelIsoCheckMem.lut')),
      ) ,
       
     OPhiExtrapolationLUTSettings = cms.PSet (
-     	charge_in_width = cms.int32(1), 
-     	etaAbs_in_width = cms.int32(8), 
-     	pTred_in_width = cms.int32(5), 
-     	out_width = cms.int32(4),
-     	filename = cms.string('data/OPhiExtrapolation.lut'),
+        charge_in_width = cms.int32(1), 
+        etaAbs_in_width = cms.int32(8), 
+        pTred_in_width = cms.int32(5), 
+        out_width = cms.int32(4),
+        filename = cms.string(os.path.join(lut_dir, 'OPhiExtrapolation.lut')),
      ) ,
       
     OvlPosSingleMatchQualLUTSettings = cms.PSet (
-     	deltaEtaRed_in_width = cms.int32(4), 
-     	deltaPhiRed_in_width = cms.int32(3), 
-     	out_width = cms.int32(1),
-     	filename = cms.string('data/OvlPosSingleMatchQual.lut'),
+        deltaEtaRed_in_width = cms.int32(4), 
+        deltaPhiRed_in_width = cms.int32(3), 
+        out_width = cms.int32(1),
+        filename = cms.string(os.path.join(lut_dir, 'OvlPosSingleMatchQual.lut')),
      ) ,
       
     FEtaExtrapolationLUTSettings = cms.PSet (
-     	charge_in_width = cms.int32(1), 
-     	etaAbsRed_in_width = cms.int32(7), 
-     	pTred_in_width = cms.int32(5), 
-     	out_width = cms.int32(4),
-     	filename = cms.string('data/FEtaExtrapolation.lut'),
+        charge_in_width = cms.int32(1), 
+        etaAbsRed_in_width = cms.int32(7), 
+        pTred_in_width = cms.int32(5), 
+        out_width = cms.int32(4),
+        filename = cms.string(os.path.join(lut_dir, 'FEtaExtrapolation.lut')),
      ) ,
       
     BOPosMatchQualLUTSettings = cms.PSet (
-     	deltaEtaRed_in_width = cms.int32(4), 
-     	deltaPhiRed_in_width = cms.int32(3), 
-     	out_width = cms.int32(1),
-     	filename = cms.string('data/BOPosMatchQual.lut'),
+        deltaEtaRed_in_width = cms.int32(4), 
+        deltaPhiRed_in_width = cms.int32(3), 
+        out_width = cms.int32(1),
+        filename = cms.string(os.path.join(lut_dir, 'BOPosMatchQual.lut')),
      ) ,
       
     OEtaExtrapolationLUTSettings = cms.PSet (
-     	charge_in_width = cms.int32(1), 
-     	etaAbsRed_in_width = cms.int32(7), 
-     	pTred_in_width = cms.int32(5), 
-     	out_width = cms.int32(4),
-     	filename = cms.string('data/OEtaExtrapolation.lut'),
+        charge_in_width = cms.int32(1), 
+        etaAbsRed_in_width = cms.int32(7), 
+        pTred_in_width = cms.int32(5), 
+        out_width = cms.int32(4),
+        filename = cms.string(os.path.join(lut_dir, 'OEtaExtrapolation.lut')),
      ) ,
       
     BEtaExtrapolationLUTSettings = cms.PSet (
-     	charge_in_width = cms.int32(1), 
-     	etaAbsRed_in_width = cms.int32(7), 
-     	pTred_in_width = cms.int32(5), 
-     	out_width = cms.int32(4),
-     	filename = cms.string('data/BEtaExtrapolation.lut'),
+        charge_in_width = cms.int32(1), 
+        etaAbsRed_in_width = cms.int32(7), 
+        pTred_in_width = cms.int32(5), 
+        out_width = cms.int32(4),
+        filename = cms.string(os.path.join(lut_dir, 'BEtaExtrapolation.lut')),
      ) ,
       
     FPhiExtrapolationLUTSettings = cms.PSet (
-     	charge_in_width = cms.int32(1), 
-     	etaAbs_in_width = cms.int32(8), 
-     	pTred_in_width = cms.int32(5), 
-     	out_width = cms.int32(4),
-     	filename = cms.string('data/FPhiExtrapolation.lut'),
+        charge_in_width = cms.int32(1), 
+        etaAbs_in_width = cms.int32(8), 
+        pTred_in_width = cms.int32(5), 
+        out_width = cms.int32(4),
+        filename = cms.string(os.path.join(lut_dir, 'FPhiExtrapolation.lut')),
      ) ,
       
     FONegMatchQualLUTSettings = cms.PSet (
-     	deltaEtaRed_in_width = cms.int32(4), 
-     	deltaPhiRed_in_width = cms.int32(3), 
-     	out_width = cms.int32(1),
-     	filename = cms.string('data/FONegMatchQual.lut'),
+        deltaEtaRed_in_width = cms.int32(4), 
+        deltaPhiRed_in_width = cms.int32(3), 
+        out_width = cms.int32(1),
+        filename = cms.string(os.path.join(lut_dir, 'FONegMatchQual.lut')),
      ) ,
       
     SortRankLUTSettings = cms.PSet (
-     	pT_in_width = cms.int32(9), 
-     	qual_in_width = cms.int32(4), 
-     	out_width = cms.int32(10),
-     	filename = cms.string('data/SortRank.lut'),
+        pT_in_width = cms.int32(9), 
+        qual_in_width = cms.int32(4), 
+        out_width = cms.int32(10),
+        filename = cms.string(os.path.join(lut_dir, 'SortRank.lut')),
      )
 )
   
