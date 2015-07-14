40dada67dfede79d7729ed7485cba15989a37023
Joschka Lingemann <joschka.lingemann@cern.ch>
2015-07-14 13:06:10 +0200

    Clean-up of producer configs.

--- L1Trigger/L1TMuon/test/microgmt_from_datfile_cfg.py
+++ L1Trigger/L1TMuon/test/microgmt_from_datfile_cfg.py
@@ -1,24 +1,27 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("L1MicroGMTInputs")
+process = cms.Process("L1uGMTEmulator")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
 # max events has to match what is in the .dat file:
-# single_muons: 108, single_tfs: 6, many_events: 91, sort_test: 7
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(91))
+# fwd_iso_scan: 10, iso_test: 20, many_events: 91
+FILENAME = "iso_test"
+n_events_dict = {"fwd_iso_scan":10, "iso_test":21, "many_events":91}
+process.maxEvents = cms.untracked.PSet(input=cms.untracked.int32(n_events_dict[FILENAME]))
 
 process.load("L1Trigger.L1TMuon.microgmtinputproducer_cfi")
+process.load("L1Trigger.L1TMuon.microgmtemulator_cfi")
 
-process.uGMTInputProducer.inputFileName = "patterns/many_events.dat"
+process.uGMTInputProducer.inputFileName = "patterns/{fname}.dat".format(fname=FILENAME)
 
-process.source = cms.Source("EmptySource",
-)
+process.source = cms.Source("EmptySource",)
+
+
 process.out = cms.OutputModule("PoolOutputModule",
-    fileName = cms.untracked.string('ugmt_input_many_events.root')
+                               fileName=cms.untracked.string('ugmt_{fname}.root'.format(fname=FILENAME))
 )
-  
-process.p = cms.Path(process.uGMTInputProducer)
+
+process.p = cms.Path(process.uGMTInputProducer+process.microGMTEmulator)
 
 process.e = cms.EndPath(process.out)
-
--- L1Trigger/L1TMuon/test/microgmt_from_mc_cfg.py
+++ L1Trigger/L1TMuon/test/microgmt_from_mc_cfg.py
@@ -28,28 +28,28 @@ if verbose:
                                         ,'HCAL'
         ),
        critical       = cms.untracked.PSet(
-                        threshold = cms.untracked.string('ERROR') 
+                        threshold = cms.untracked.string('ERROR')
         ),
        detailedInfo   = cms.untracked.PSet(
-                      threshold  = cms.untracked.string('INFO'), 
-                      CondDBESSource  = cms.untracked.PSet (limit = cms.untracked.int32(0) ), 
-                      EventSetupDependency  = cms.untracked.PSet (limit = cms.untracked.int32(0) ), 
-                      Geometry  = cms.untracked.PSet (limit = cms.untracked.int32(0) ), 
-                      MuonGeom  = cms.untracked.PSet (limit = cms.untracked.int32(0) ), 
-                      Alignment  = cms.untracked.PSet (limit = cms.untracked.int32(0) ), 
-                      GetManyWithoutRegistration  = cms.untracked.PSet (limit = cms.untracked.int32(0) ), 
-                      GetByLabelWithoutRegistration  = cms.untracked.PSet (limit = cms.untracked.int32(0) ) 
+                      threshold  = cms.untracked.string('INFO'),
+                      CondDBESSource  = cms.untracked.PSet (limit = cms.untracked.int32(0) ),
+                      EventSetupDependency  = cms.untracked.PSet (limit = cms.untracked.int32(0) ),
+                      Geometry  = cms.untracked.PSet (limit = cms.untracked.int32(0) ),
+                      MuonGeom  = cms.untracked.PSet (limit = cms.untracked.int32(0) ),
+                      Alignment  = cms.untracked.PSet (limit = cms.untracked.int32(0) ),
+                      GetManyWithoutRegistration  = cms.untracked.PSet (limit = cms.untracked.int32(0) ),
+                      GetByLabelWithoutRegistration  = cms.untracked.PSet (limit = cms.untracked.int32(0) )
 
        ),
        cout   = cms.untracked.PSet(
-                threshold  = cms.untracked.string('INFO'), 
-                CondDBESSource  = cms.untracked.PSet (limit = cms.untracked.int32(0) ), 
-                EventSetupDependency  = cms.untracked.PSet (limit = cms.untracked.int32(0) ), 
-                Geometry  = cms.untracked.PSet (limit = cms.untracked.int32(0) ), 
-                MuonGeom  = cms.untracked.PSet (limit = cms.untracked.int32(0) ), 
-                Alignment  = cms.untracked.PSet (limit = cms.untracked.int32(0) ), 
-                GetManyWithoutRegistration  = cms.untracked.PSet (limit = cms.untracked.int32(0) ), 
-                GetByLabelWithoutRegistration  = cms.untracked.PSet (limit = cms.untracked.int32(0) ) 
+                threshold  = cms.untracked.string('INFO'),
+                CondDBESSource  = cms.untracked.PSet (limit = cms.untracked.int32(0) ),
+                EventSetupDependency  = cms.untracked.PSet (limit = cms.untracked.int32(0) ),
+                Geometry  = cms.untracked.PSet (limit = cms.untracked.int32(0) ),
+                MuonGeom  = cms.untracked.PSet (limit = cms.untracked.int32(0) ),
+                Alignment  = cms.untracked.PSet (limit = cms.untracked.int32(0) ),
+                GetManyWithoutRegistration  = cms.untracked.PSet (limit = cms.untracked.int32(0) ),
+                GetByLabelWithoutRegistration  = cms.untracked.PSet (limit = cms.untracked.int32(0) )
                 ),
                                         )
 
@@ -60,10 +60,22 @@ process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
 
 process.source = cms.Source(
     'PoolSource',
-    fileNames = cms.untracked.vstring('file:/afs/cern.ch/work/j/jlingema/public/omtf_input_test.root')
+    fileNames = cms.untracked.vstring(
+      # 'file:/afs/cern.ch/work/j/jlingema/public/omtf_input_test.root'
+      '/store/relval/CMSSW_7_5_0_pre1/RelValTTbar_13/GEN-SIM-DIGI-RAW-HLTDEBUG/MCRUN2_74_V7-v1/00000/1431747F-84E3-E411-9537-0025905B85D8.root',
+      '/store/relval/CMSSW_7_5_0_pre1/RelValTTbar_13/GEN-SIM-DIGI-RAW-HLTDEBUG/MCRUN2_74_V7-v1/00000/1892A9F5-41E3-E411-811B-0026189438A7.root',
+      '/store/relval/CMSSW_7_5_0_pre1/RelValTTbar_13/GEN-SIM-DIGI-RAW-HLTDEBUG/MCRUN2_74_V7-v1/00000/36862409-75E2-E411-ACC6-00261894393F.root',
+      '/store/relval/CMSSW_7_5_0_pre1/RelValTTbar_13/GEN-SIM-DIGI-RAW-HLTDEBUG/MCRUN2_74_V7-v1/00000/3E6E9C77-84E3-E411-86C7-0025905A4964.root',
+      '/store/relval/CMSSW_7_5_0_pre1/RelValTTbar_13/GEN-SIM-DIGI-RAW-HLTDEBUG/MCRUN2_74_V7-v1/00000/5CF8490F-75E2-E411-83D2-00261894398C.root',
+      '/store/relval/CMSSW_7_5_0_pre1/RelValTTbar_13/GEN-SIM-DIGI-RAW-HLTDEBUG/MCRUN2_74_V7-v1/00000/A496F864-84E3-E411-8ECE-003048FFD752.root',
+      '/store/relval/CMSSW_7_5_0_pre1/RelValTTbar_13/GEN-SIM-DIGI-RAW-HLTDEBUG/MCRUN2_74_V7-v1/00000/B2F7971B-75E2-E411-9262-00261894380D.root',
+      '/store/relval/CMSSW_7_5_0_pre1/RelValTTbar_13/GEN-SIM-DIGI-RAW-HLTDEBUG/MCRUN2_74_V7-v1/00000/C20BD695-92E3-E411-A02C-002618943914.root',
+      '/store/relval/CMSSW_7_5_0_pre1/RelValTTbar_13/GEN-SIM-DIGI-RAW-HLTDEBUG/MCRUN2_74_V7-v1/00000/C80A8C08-75E2-E411-8FDC-002618943810.root',
+      '/store/relval/CMSSW_7_5_0_pre1/RelValTTbar_13/GEN-SIM-DIGI-RAW-HLTDEBUG/MCRUN2_74_V7-v1/00000/F604DB80-84E3-E411-B32F-0025905A60EE.root',
+      )
     )
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(500))
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100))
 
 ###PostLS1 geometry used
 process.load('Configuration.Geometry.GeometryExtendedPostLS1Reco_cff')
@@ -86,8 +98,11 @@ process.L1TMuonSeq = cms.Sequence( process.uGMTInputProducer +
 process.L1TMuonPath = cms.Path(process.L1TMuonSeq)
 
 
-process.out = cms.OutputModule("PoolOutputModule", 
-   fileName = cms.untracked.string("ugmt_from_mc_test.root"),
+process.out = cms.OutputModule("PoolOutputModule",
+  outputCommands = cms.untracked.vstring(
+              'drop *',
+              'keep *_*_*_L1TMuonEmulation'),
+   fileName = cms.untracked.string("ugmt_ttbar_small_sample.root"),
 )
 
 process.output_step = cms.EndPath(process.out)
